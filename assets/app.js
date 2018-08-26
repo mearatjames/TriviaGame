b2748758b51494570d5595f4d1081e5564af564a
Mearat Hou <mearatjames@gmail.com>
2018-08-25 21:51:41 -0700

    Add basic layout of the game

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+
+
+$(function() {
+   $('#gameDiv').hide();
+});
+
+$('#start').on('click', function() {
+    $('#firstPage').remove();
+    $('#gameDiv').show();
+})
+
+
+
+$('.card-action').on('click', function(){
+    
+})
